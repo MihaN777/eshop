e79dad15dc15fb45e97908261af6e1fb94ec1b4f
MihaN777 <mihail777mails@yandex.ru>
2025-09-22 13:01:23 +0400

    Настройка проекта

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Connection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +23,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Отключить отложенную загрузку
+        Model::preventLazyLoading(!app()->isProduction());
+
+        // Когда попытаетесь присвоить значение, который не был добавлен в массив fillable модели - exception
+        Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
+
+        // Мониторинг общего времени выполнения запроса
+        DB::whenQueryingForLongerThan(500, function (Connection $connection, QueryExecuted $event) {
+            // TODO log
+        });
     }
 }
