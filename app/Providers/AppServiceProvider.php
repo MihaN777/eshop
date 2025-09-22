9d2bc6e0028aac038d931c86e0a368e45d95ab70
MihaN777 <mihail777mails@yandex.ru>
2025-09-22 16:08:06 +0400

    Добавлено ограничение количества запросов

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,15 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Database\Connection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\HttpFoundation\Response;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -32,6 +36,20 @@ class AppServiceProvider extends ServiceProvider
         // Мониторинг общего времени выполнения запроса
         DB::whenQueryingForLongerThan(500, function (Connection $connection, QueryExecuted $event) {
             // TODO log
+        });
+
+        // Ограничение количества запросов
+        RateLimiter::for('global', function (Request $request) {
+            return Limit::perMinute(500)
+                ->by($request->user()?->id ?: $request->ip())
+                ->response(function (Request $request, array $headers) {
+                    return response('TOO_MANY_REQUESTS', Response::HTTP_TOO_MANY_REQUESTS, $headers);
+                });
+        });
+
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)
+                ->by($request->user()?->id ?: $request->ip());
         });
     }
 }
