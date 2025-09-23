bd28ec9fa57a1278ed10f2f069f00eef64bb3da3
MihaN777 <mihail777mails@yandex.ru>
2025-09-23 15:14:58 +0400

    Добавлен возврящаемый тип в TelegramBotApi.php

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -8,7 +8,7 @@ class TelegramBotApi
 {
     // https://api.telegram.org/botXXXXXXtokenXXXXXXX/getUpdates - bot info
     public const HOST = 'https://api.telegram.org/bot';
-    public static function sendMessage(string $token, int $chatId, string $message = '') {
+    public static function sendMessage(string $token, int $chatId, string $message = ''): void {
         Http::get(self::HOST . $token . '/sendMessage', [
             'chat_id' => $chatId,
             'text' => $message,
