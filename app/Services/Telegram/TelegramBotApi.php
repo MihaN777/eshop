4dd37409e06592d554d50b91984c702cc36bc72f
MihaN777 <mihail777mails@yandex.ru>
2025-09-24 16:16:22 +0400

    Добавлен try catch в TelegramBotApi.php

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -2,16 +2,30 @@
 
 namespace App\Services\Telegram;
 
+use App\Services\Telegram\Exceptions\TelegramBotApiException;
 use Illuminate\Support\Facades\Http;
+use Throwable;
 
 class TelegramBotApi
 {
     // https://api.telegram.org/botXXXXXXtokenXXXXXXX/getUpdates - bot info
     public const HOST = 'https://api.telegram.org/bot';
-    public static function sendMessage(string $token, int $chatId, string $message = ''): void {
-        Http::get(self::HOST . $token . '/sendMessage', [
-            'chat_id' => $chatId,
-            'text' => $message,
-        ]);
+
+    public static function sendMessage(string $token, int $chatId, string $message = ''): bool
+    {
+        try {
+            $response = Http::get(self::HOST . $token . '/sendMessage',
+                [
+                    'chat_id' => $chatId,
+                    'text' => $message,
+                ])
+                ->throw()
+                ->json();
+
+            return $response['ok'] ?? false;
+        } catch (Throwable $e) {
+            report(new TelegramBotApiException($e->getMessage()));
+            return false;
+        }
     }
 }
