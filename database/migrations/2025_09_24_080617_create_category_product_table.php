addbafa577f190f59033469792082908d876447a
MihaN777 <mihail777mails@yandex.ru>
2025-09-24 12:39:29 +0400

    Создана пивот таблица для категорий и продуктов

--- database/migrations/2025_09_24_080617_create_category_product_table.php
+++ database/migrations/2025_09_24_080617_create_category_product_table.php
@@ -4,22 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('category_product', function (Blueprint $table) {
             $table->id();
-            $table->string('slug');
-            $table->string('title');
-            $table->unsignedInteger('price');
-            $table->string('image')->nullable();
 
-            $table->foreignId('brand_id')
+            $table->foreignId('category_id')
                 ->index()
-                ->constrained('brands')
+                ->constrained('categories')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->foreignId('product_id')
+                ->index()
+                ->constrained('products')
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
 
@@ -32,6 +35,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        if (app()->isLocal()) Schema::dropIfExists('products');
+        if (app()->isLocal()) Schema::dropIfExists('category_product');
     }
 };
