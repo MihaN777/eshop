32abf5a6cc02cf4a12ed103f96bf612ca56c6641
MihaN777 <mihail777mails@yandex.ru>
2025-09-28 14:15:16 +0400

    Добавлен роут login

---
+++ resources/views/layouts/auth-layout.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title', config('app.name'))</title>
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite([
+            'resources/css/app.css',
+            'resources/sass/main.sass',
+            'resources/js/app.js'
+            ])
+    @endif
+</head>
+<body class="antialiased">
+
+<main class="md:min-h-screen md:flex md:items-center md:justify-center py-16 lg:py-20">
+    <div class="container">
+        <div class="text-center">
+            <a href="{{ route('home')  }}" class="inline-block" rel="home">
+                <img src="{{ Vite::image('logo.svg') }}"
+                     class="w-[148px] md:w-[201px] h-[36px] md:h-[50px]" alt="CutCode">
+            </a>
+        </div>
+
+        @yield('content')
+    </div>
+</main>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/login', [HomeController::class, 'login'])->name('login');
 
 //Route::get('/dashboard', function () {
 //    return view('dashboard');
