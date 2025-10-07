c1e00e4895ee8d3567c48e329edc66034e964a51
MihaN777 <mihail777mails@yandex.ru>
2025-10-07 14:09:11 +0400

    Изменено сообщение по сбросу пароля

--- app/Notifications/AuthCustom/ResetPasswordNotification.php
+++ app/Notifications/AuthCustom/ResetPasswordNotification.php
@@ -14,11 +14,24 @@ class ResetPasswordNotification extends ResetPassword implements ShouldQueue
     //        public function __construct($token)
     //        {
     //            // Переопределить очередь
-    //            $this->queue = 'verify';
+    //            $this->queue = 'reset';
     //
     //            // Переопределить соединение
-    //            $this->connection = 'verify';
+    //            $this->connection = 'reset';
     //
     //            parent::__construct($token);
     //        }
+
+    protected function buildMailMessage($url): MailMessage
+    {
+        $appName = config('app.name');
+        $expire = config('auth.passwords.' . config('auth.defaults.passwords') . '.expire');
+
+        return (new MailMessage)
+            ->subject("{$appName} : Обновление пароля")
+            ->line('Вы получили это электронное письмо, потому что был получен запрос на обновление пароля для вашей учетной записи.')
+            ->action('Обновить', $url)
+            ->line("Срок действия этой ссылки для обновления пароля истечет через {$expire} минут.")
+            ->line('Если вы не запрашивали обновление пароля, никаких дальнейших действий не требуется.');
+    }
 }
