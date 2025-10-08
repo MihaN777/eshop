e71ba4c75e8de20fcec68aa6dd3b3d51b2cafbf0
MihaN777 <mihail777mails@yandex.ru>
2025-10-08 14:13:42 +0400

    Добавлено оповещение для нового пользователя

---
+++ app/Notifications/NewUserNotification.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NewUserNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $appName = config('app.name');
+
+        return (new MailMessage)
+            ->subject("{$appName}: Добро пожаловать!")
+            ->line('Вам начислены балы для скидки. Успейте ими воспользоваться!')
+            ->line('Нажмите на кнопку ниже чтобы узнать подробности.')
+            ->action('Ваш профиль', url(route('profile')));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
