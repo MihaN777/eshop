660b134bf825868ac0618f3caf373c073b31d5ac
MihaN777 <mihail777mails@yandex.ru>
2025-10-17 16:47:01 +0400

    Рефакторинг трейтов

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Traits\Models\HasSlug;
+use App\Support\Traits\Models\HasSlug;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -15,6 +15,7 @@ class Category extends Model
     use HasSlug;
 
     protected $fillable = [
+        'slug',
         'title',
         'on_home_page',
         'sorting',
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Traits\Models\HasSlug;
+use App\Support\Traits\Models\HasSlug;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
