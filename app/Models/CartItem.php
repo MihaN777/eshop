42d40df137aaa29e9d47a5662b6bde142a2c8b0b
MihaN777 <mihail777mails@yandex.ru>
2025-11-10 12:54:19 +0400

    Добавлены отношения для CartItem

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class CartItem extends Model
 {
@@ -20,5 +21,14 @@ class CartItem extends Model
     public function cart(): BelongsTo
     {
         return $this->belongsTo(Cart::class);
+    }
+
+    public function product(): BelongsTo {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function optionValues(): BelongsToMany
+    {
+        return $this->belongsToMany(OptionValue::class, 'cart_item_option_value', 'cart_item_id', 'option_value_id');
     }
 }
