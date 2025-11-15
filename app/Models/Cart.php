307d5df4ca8a669bff27d192f84c2818bc0225d9
MihaN777 <mihail777mails@yandex.ru>
2025-11-15 16:43:37 +0400

    Добавлена очистка корзины от старых записей

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,11 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\MassPrunable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cart extends Model
 {
+    // Очистка модели от старых записей
+    use Prunable;
+
+    // Массовая очистка модели от старых записей
+    // use MassPrunable;
+
     protected $fillable = [
         'storage_id',
         'user_id',
@@ -18,4 +27,18 @@ class Cart extends Model
     {
         return $this->hasMany(CartItem::class);
     }
+
+    // Функции модели
+
+    public function prunable(): Builder
+    {
+        // Запрос для очистка модели
+        return static::query()->where('created_at', '<=', now()->subDay());
+    }
+
+    // protected function pruning(): void
+    // {
+    //     // Метод вызываемый перед очистком модели (не вызывается при массовой очистке)
+    //     // $this->deleteFile;
+    // }
 }
