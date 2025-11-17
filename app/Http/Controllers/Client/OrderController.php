3f3379b550447531b2fc46229b509ee7e41e55b5
MihaN777 <mihail777mails@yandex.ru>
2025-11-17 15:36:59 +0400

    Рефакторинг OrderController и view

--- app/Http/Controllers/Client/OrderController.php
+++ app/Http/Controllers/Client/OrderController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Models\DeliveryType;
+use App\Models\PaymentMethod;
+use App\Support\Exceptions\ProjectException;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 
@@ -10,13 +13,19 @@ class OrderController extends Controller
 {
     public function order(): View
     {
+        $cartItems = cart()->items();
+
+        if ($cartItems->isEmpty()) throw new ProjectException('Корзина пуста');
+
         return view('client.order', [
-            'cartItems' => cart()->items(),
+            'cartItems' => $cartItems,
+            'payments' => PaymentMethod::query()->get(),
+            'deliveries' => DeliveryType::query()->get(),
         ]);
     }
 
     public function handle(): RedirectResponse
     {
-        return redirect()->route('order');
+        return redirect()->route('profile');
     }
 }
