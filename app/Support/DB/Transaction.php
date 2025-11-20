fe17ab5409a5b9806a858c53a27cb1d703c3f11e
MihaN777 <mihail777mails@yandex.ru>
2025-11-20 13:40:15 +0400

    Добавлен класс Transaction

---
+++ app/Support/DB/Transaction.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Support\DB;
+
+use Closure;
+use Illuminate\Support\Facades\DB;
+use Throwable;
+
+class Transaction
+{
+    public static function run(
+        Closure $callback,
+        Closure $finished = null,
+        Closure $onError = null,
+    ): mixed
+    {
+        try {
+            DB::beginTransaction();
+
+            $result = $callback();
+
+            if (!is_null($finished)) $finished($result);
+
+            DB::commit();
+
+            return $result;
+        } catch (Throwable $e) {
+            DB::rollBack();
+
+            if (!is_null($onError)) $onError($e);
+
+            throw $e;
+        }
+    }
+}
