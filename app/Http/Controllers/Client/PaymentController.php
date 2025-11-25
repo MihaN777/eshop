9e4f471aacab3b987153893a986d02207c903e27
MihaN777 <mihail777mails@yandex.ru>
2025-11-25 15:56:59 +0400

    Рефакторинг PaymentController

--- app/Http/Controllers/Client/PaymentController.php
+++ app/Http/Controllers/Client/PaymentController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Client;
 
+use App\Domains\Order\States\Payment\PaidPaymentState;
 use App\Http\Controllers\Controller;
+use App\Models\Payment;
 use App\Support\Payment\PaymentSystem;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Throwable;
@@ -14,6 +17,16 @@ class PaymentController extends Controller
     {
         try {
             $response = PaymentSystem::validate()->response();
+
+            // $paymentId = $request->get("payment_id");
+            //
+            // $payment = Payment::query()
+            //     ->where('payment_id', $paymentId)
+            //     ->first();
+            //
+            // if (!$payment) throw new Exception('Payment not found');
+            //
+            // $payment->state->transitionTo(PaidPaymentState::class);
         } catch (Throwable $e) {
             return response()->json(
                 data: ['error' => $e->getMessage()],
