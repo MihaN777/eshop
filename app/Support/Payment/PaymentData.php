9163735a175d29278a4baa2dc36e4ba172f9d09d
MihaN777 <mihail777mails@yandex.ru>
2025-11-28 13:03:02 +0400

    Рефакторинг PaymentData

--- app/Support/Payment/PaymentData.php
+++ app/Support/Payment/PaymentData.php
@@ -5,15 +5,38 @@ namespace App\Support\Payment;
 use App\Support\ValueObjects\Price;
 use Illuminate\Support\Collection;
 
-class PaymentData
+final class PaymentData
 {
     public function __construct(
-        public string     $id,
-        public string     $description,
-        public string     $return_url,
-        public Price      $amount,
-        public Collection $meta
+        public readonly int        $order_id,
+        public readonly ?string    $pay_id,
+        public readonly string     $description,
+        public readonly string     $return_url,
+        public readonly Price      $amount,
+        public readonly Collection $meta
     )
     {
+    }
+
+    public function toCollection(): Collection
+    {
+        return collect([
+            'order_id' => $this->order_id,
+            'pay_id' => $this->pay_id,
+            'description' => $this->description,
+            'return_url' => $this->return_url,
+            'amount' => $this->amount,
+            'meta' => $this->meta,
+        ]);
+    }
+
+    public function toArray(): array
+    {
+        return $this->toCollection()->toArray();
+    }
+
+    public function toJson(): string
+    {
+        return $this->toCollection()->toJson();
     }
 }
