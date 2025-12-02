b0646ea1be903bc7da4161f8d39174184ed5c515
MihaN777 <mihail777mails@yandex.ru>
2025-12-02 13:59:23 +0400

    Рефакторинг PaymentController

--- app/Http/Controllers/Client/PaymentController.php
+++ app/Http/Controllers/Client/PaymentController.php
@@ -5,14 +5,15 @@ namespace App\Http\Controllers\Client;
 use App\Http\Controllers\Controller;
 use App\Support\Payment\PaymentSystem;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Throwable;
 
 class PaymentController extends Controller
 {
-    public function __invoke(Request $request): JsonResponse
+    public function __invoke(string $provider): JsonResponse
     {
         try {
+            PaymentSystem::setProviderByName($provider);
+
             $response = PaymentSystem::validate()->response();
         } catch (Throwable $e) {
             return response()->json(
