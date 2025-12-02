6527b4a349ed53bc8c400280aa93bff92753bff4
MihaN777 <mihail777mails@yandex.ru>
2025-12-02 13:49:32 +0400

    Изменен PaymentServiceProvider

--- app/Providers/PaymentServiceProvider.php
+++ app/Providers/PaymentServiceProvider.php
@@ -24,17 +24,17 @@ class PaymentServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        try {
-            PaymentSystem::provider(function () {
-                // if (request()->has('unit_pay'))
-                //     return new UnitPay(config('payment.providers.unit_pay'));
-
-                return new YooKassa(config('payment.providers.yoo_kassa'));
-            });
-        } catch (Throwable $e) {
-            logger()->error($e->getMessage());
-            logger()->channel('telegram')->error($e->getMessage());
-        }
+        // try {
+        //     PaymentSystem::provider(function () {
+        //         // if (request()->has('unit_pay'))
+        //         //     return new UnitPay(config('payment.providers.unit_pay'));
+        //
+        //         return new YooKassa(config('payment.providers.yoo_kassa'));
+        //     });
+        // } catch (Throwable $e) {
+        //     logger()->error($e->getMessage());
+        //     logger()->channel('telegram')->error($e->getMessage());
+        // }
 
         PaymentSystem::onCreating(function (PaymentData $paymentData) {
             return $paymentData;
