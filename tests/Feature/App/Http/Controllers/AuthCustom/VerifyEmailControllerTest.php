435908e32d33151476d1471f0818f567aef07f3e
MihaN777 <mihail777mails@yandex.ru>
2026-06-16 15:29:00 +0400

    Добавлены тест на страницу подтверждения email

---
+++ tests/Feature/App/Http/Controllers/AuthCustom/VerifyEmailControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\App\Http\Controllers\AuthCustom;
+
+use App\Http\Controllers\AuthCustom\VerifyEmailController;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class VerifyEmailControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testIndex(): void
+    {
+        $user = User::factory()->create(
+            [
+                'email' => 'test@vk.com',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        $this->actingAs($user);
+
+        $this->get(action([VerifyEmailController::class, 'emailNotice']))
+            ->assertOk()
+            ->assertSee('Подтверждение электронной почты')
+            ->assertViewIs('auth_custom.verify-email');
+    }
+}
