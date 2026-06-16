225926aa2a7bc81c028a5d5d9d718923f7260d36
MihaN777 <mihail777mails@yandex.ru>
2026-06-16 15:48:38 +0400

    Добавлены тест на отправку оповещения для подтверждения email

--- tests/Feature/App/Http/Controllers/AuthCustom/VerifyEmailControllerTest.php
+++ tests/Feature/App/Http/Controllers/AuthCustom/VerifyEmailControllerTest.php
@@ -4,8 +4,10 @@ namespace Tests\Feature\App\Http\Controllers\AuthCustom;
 
 use App\Http\Controllers\AuthCustom\VerifyEmailController;
 use App\Models\User;
+use App\Notifications\AuthCustom\VerifyEmailNotification;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Notification;
 use Tests\TestCase;
 
 class VerifyEmailControllerTest extends TestCase
@@ -27,5 +29,24 @@ class VerifyEmailControllerTest extends TestCase
             ->assertOk()
             ->assertSee('Подтверждение электронной почты')
             ->assertViewIs('auth_custom.verify-email');
+    }
+
+    public function testEmailSend(): void {
+        Notification::fake();
+
+        $user = User::factory()->create(
+            [
+                'email' => 'test@vk.com',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        $this->actingAs($user);
+
+        $response = $this->post(action([VerifyEmailController::class, 'emailSend']));
+
+        Notification::assertSentTo($user, VerifyEmailNotification::class);
+
+        $response->assertRedirect();
     }
 }
