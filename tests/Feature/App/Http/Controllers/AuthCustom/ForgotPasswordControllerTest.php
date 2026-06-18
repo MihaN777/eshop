229626286a6ed13ae14a2a4474f5102f361a6b24
MihaN777 <mihail777mails@yandex.ru>
2026-06-18 15:33:55 +0400

    Добавлены тест на отправку ссылки для сброса пароля

--- tests/Feature/App/Http/Controllers/AuthCustom/ForgotPasswordControllerTest.php
+++ tests/Feature/App/Http/Controllers/AuthCustom/ForgotPasswordControllerTest.php
@@ -3,7 +3,11 @@
 namespace Tests\Feature\App\Http\Controllers\AuthCustom;
 
 use App\Http\Controllers\AuthCustom\ForgotPasswordController;
+use App\Models\User;
+use App\Notifications\AuthCustom\ResetPasswordNotification;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Notification;
 use Tests\TestCase;
 
 class ForgotPasswordControllerTest extends TestCase
@@ -16,5 +20,30 @@ class ForgotPasswordControllerTest extends TestCase
             ->assertOk()
             ->assertSee('Забыли пароль')
             ->assertViewIs('auth_custom.forgot-password');
+    }
+
+    public function test_forgot_password_send_success(): void
+    {
+        Notification::fake();
+
+        $user = User::factory()->create(
+            [
+                'email' => 'test@vk.com',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        $this->post(
+            action([ForgotPasswordController::class, 'forgotPasswordSend']),
+            ['email' => $user->email]
+        )->assertRedirect();
+
+        Notification::assertSentTo($user, ResetPasswordNotification::class, function ($notification) {
+            $response = $this->get(route('password.reset', ['token' => $notification->token]));
+
+            $response->assertOk();
+
+            return true;
+        });
     }
 }
