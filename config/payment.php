88e7e1a099607e4ac790094dbf114d09274b8342
MihaN777 <mihail777mails@yandex.ru>
2026-07-01 14:56:53 +0400

    Fix лишней обертки

--- config/payment.php
+++ config/payment.php
@@ -6,21 +6,19 @@ use Tests\Support\Payment\FakePaymentProvider;
 return [
 
     'providers' => [
-        [
-            'production' => [
-                'yoo_kassa' => [
-                    'class' => YooKassa::class,
-                    'key' => env('PAYMENT_YOOKASSA_KEY', ''),
-                    'shop_id' => env('PAYMENT_YOOKASSA_SHOP_ID', ''),
-                ],
+        'production' => [
+            'yoo_kassa' => [
+                'class' => YooKassa::class,
+                'key' => env('PAYMENT_YOOKASSA_KEY', ''),
+                'shop_id' => env('PAYMENT_YOOKASSA_SHOP_ID', ''),
             ],
+        ],
 
-            'testing' => [
-                'fake' => [
-                    'class' => FakePaymentProvider::class,
-                    'key' => env('PAYMENT_FAKE_KEY', ''),
-                    'shop_id' => env('PAYMENT_FAKE_SHOP_ID', ''),
-                ],
+        'testing' => [
+            'fake' => [
+                'class' => FakePaymentProvider::class,
+                'key' => env('PAYMENT_FAKE_KEY', ''),
+                'shop_id' => env('PAYMENT_FAKE_SHOP_ID', ''),
             ],
         ],
     ],
