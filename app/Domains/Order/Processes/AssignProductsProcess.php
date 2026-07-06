6c12dcf9bbef5e01c832789ff186b4ed84eb4575
MihaN777 <mihail777mails@yandex.ru>
2026-07-06 15:24:21 +0400

    Добавлен перенос данных из cart_item_option_value в order_item_option_value в процессе создания заказа AssignProductsProcess

--- app/Domains/Order/Processes/AssignProductsProcess.php
+++ app/Domains/Order/Processes/AssignProductsProcess.php
@@ -11,17 +11,17 @@ class AssignProductsProcess implements OrderProcessContract
     {
         $cart = cart();
 
-        $order->orderItems()->createMany(
-            $cart->items()->map(
-                function ($item) {
-                    return [
-                        'product_id' => $item->product_id,
-                        'price' => $item->price,
-                        'quantity' => $item->quantity,
-                    ];
-                }
-            )->toArray()
-        );
+        foreach ($cart->items() as $item) {
+            $orderItem = $order->orderItems()->create([
+                'product_id' => $item->product_id,
+                'price' => $item->price,
+                'quantity' => $item->quantity,
+            ]);
+            
+            $orderItem->optionValues()->attach(
+                $item->optionValues->pluck('id')->all()
+            );
+        }
 
         $order->load(['orderItems.product']);
         $order->amount = $cart->amount();
